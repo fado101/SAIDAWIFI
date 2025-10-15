5379d9d7e90bbcc8a8f17afee38e7ac8588b43f2
fado101 <154098402+fado101@users.noreply.github.com>
2025-10-15 09:50:18 +0300

    Update toaster.tsx

--- client/src/components/ui/toaster.tsx
+++ client/src/components/ui/toaster.tsx
@@ -1,9 +1,5 @@
-// client/src/components/ui/toaster.tsx
 import { Toaster } from 'sonner'
-
 export function ToasterProvider() {
   return <Toaster richColors position="top-center" />
 }
-
-// للتوافق مع بعض الأكواد التي تستورد { Toaster } مباشرة
 export { Toaster }
